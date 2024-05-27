5189d5c9a754b146f3f4679b1432c2ec249ac233
Mateusz <mateusz.gajos@asseco.pl>
2024-05-27 09:33:27 +0200

    optimize code readability

--- Main.java
+++ Main.java
@@ -11,29 +11,31 @@ public class Main {
         findLargestSum(items, capacity);
     }
 
-
-
+    // https://en.wikipedia.org/wiki/Knapsack_problem, but decided to go for own algorithm
+    // which finds an optimal solution
     static Set<Item> findLargestSum(Set<Item> items, int initialCapacity) {
-        if(items.isEmpty()){
+        if (items.isEmpty()) {
             throw new IllegalArgumentException("Set should not be empty");
         }
-        if (items.size() == 1){
+        if (items.size() == 1) {
             return items;
         }
-
-        var itemsByValueDesc = items.stream().sorted((i1, i2) -> i2.getValue() - i1.getValue()).collect(Collectors.toList());
-        var left1WeightItems = new ArrayList<Item>();
+        // O(nlogn)
+        var itemsByValueDesc
+            = items.stream().sorted((i1, i2) -> i2.getValue() - i1.getValue()).collect(Collectors.toList());
+        var itemsWithWeight1Left = new ArrayList<Item>();
         var usedCapacity = 0;
         var resultList = new ArrayList<Item>();
+
         for (Item item : itemsByValueDesc) {
             if (initialCapacity - usedCapacity < 1) {
-                if (item.getWeight() == 1) {
-                    left1WeightItems.add(item);
+                if (item.isOfWeight(1)) {
+                    itemsWithWeight1Left.add(item);
                 }
                 continue;
             }
             if (initialCapacity - usedCapacity == 1) {
-                resultList.add(itemsByValueDesc.stream().filter(i -> i.getWeight() == 1).findFirst().get());
+                resultList.add(itemsByValueDesc.stream().filter(i -> i.isOfWeight(1)).findFirst().get());
                 usedCapacity += item.getWeight();
                 continue;
             }
@@ -41,64 +43,72 @@ public class Main {
             usedCapacity += item.getWeight();
         }
 
-        try {
-            if(left1WeightItems.size() >= 2){
-                swapItemsWithHigherValue(left1WeightItems, resultList);
-            }
-        } catch (IndexOutOfBoundsException e ){
-            e.getStackTrace();
-        }
+        // O(n^2) - bottleneck is here, but we have a guarantee of finding an optimal solution
+        swapItemsWithHigherValue(itemsWithWeight1Left, resultList);
 
-         return resultList.stream().collect(Collectors.toSet());
+        return resultList.stream().collect(Collectors.toSet());
     }
 
-    private static void swapItemsWithHigherValue(ArrayList<Item> left1WeightItems, ArrayList<Item> resultList) {
+    private static void swapItemsWithHigherValue(ArrayList<Item> itemsWithWeight1Left, ArrayList<Item> resultList) {
+        if (itemsWithWeight1Left.size() < 2) {
+            return;
+        }
+
+        if (resultList.stream().noneMatch(item -> item.isOfWeight(2))) {
+            return;
+        }
         var lowestValue2WeightItem = getLowestValue2WeightItem(resultList);
-        var highestValue1WeightItem = left1WeightItems.get(0);
-        var secondHighestValue1WeightItem = left1WeightItems.get(1);
-        while (lowest2WeightItemHasLessValueThan2Highest1WeightItems(highestValue1WeightItem,secondHighestValue1WeightItem, lowestValue2WeightItem)){
-            if(lowestValue2WeightItem.getValue() == 0){
-                return;
-            }
-            resultList.remove(lowestValue2WeightItem);
+        var highestValue1WeightItem = itemsWithWeight1Left.get(0);
+        var secondHighestValue1WeightItem = itemsWithWeight1Left.get(1);
 
-            resultList.add(secondHighestValue1WeightItem);
-            resultList.add(highestValue1WeightItem);
+        while (lowestValue2WeightItem.isPresent()
+            && lowest2WeightItemHasLessValueThan2Highest1WeightItems(highestValue1WeightItem,
+                secondHighestValue1WeightItem, lowestValue2WeightItem.get())) {
 
-            left1WeightItems.remove(highestValue1WeightItem);
-            left1WeightItems.remove(secondHighestValue1WeightItem);
+            swapItems(itemsWithWeight1Left, resultList, lowestValue2WeightItem.get(), highestValue1WeightItem,
+                secondHighestValue1WeightItem);
 
             lowestValue2WeightItem = getLowestValue2WeightItem(resultList);
-            highestValue1WeightItem = left1WeightItems.get(0);
-            secondHighestValue1WeightItem = left1WeightItems.get(1);
-
-
+            if (itemsWithWeight1Left.size() < 2) {
+                break;
+            }
+            highestValue1WeightItem = itemsWithWeight1Left.get(0);
+            secondHighestValue1WeightItem = itemsWithWeight1Left.get(1);
         }
     }
 
-    private static boolean lowest2WeightItemHasLessValueThan2Highest1WeightItems(Item highestValue1WeightItem, Item secondHighestValue1WeightItem, Item lowestValue2WeightItem) {
-        return highestValue1WeightItem.getValue() + secondHighestValue1WeightItem.getValue() > lowestValue2WeightItem.getValue();
+    private static void swapItems(ArrayList<Item> itemsWithWeight1Left, ArrayList<Item> resultList,
+        Item lowestValue2WeightItem, Item highestValue1WeightItem, Item secondHighestValue1WeightItem) {
+        resultList.remove(lowestValue2WeightItem);
+
+        resultList.add(secondHighestValue1WeightItem);
+        resultList.add(highestValue1WeightItem);
+
+        itemsWithWeight1Left.remove(highestValue1WeightItem);
+        itemsWithWeight1Left.remove(secondHighestValue1WeightItem);
     }
 
-    private static Item getLowestValue2WeightItem(ArrayList<Item> resultList) {
-        return resultList.stream().filter(item -> item.getWeight() == 2).min(Comparator.comparing(Item::getValue)).orElseGet(() -> new Item((short) 2,0));
+    private static boolean lowest2WeightItemHasLessValueThan2Highest1WeightItems(Item highestValue1WeightItem,
+        Item secondHighestValue1WeightItem, Item lowestValue2WeightItem) {
+        return highestValue1WeightItem.getValue() + secondHighestValue1WeightItem.getValue() > lowestValue2WeightItem
+            .getValue();
+    }
+
+    private static Optional<Item> getLowestValue2WeightItem(ArrayList<Item> resultList) {
+        return resultList.stream().filter(item -> item.getWeight() == 2).min(Comparator.comparing(Item::getValue));
     }
 
     private static Set<Item> createItems() {
         Set<Item> items = new HashSet<>();
 
-//        short[] weights = {1, 1, 1, 1, 1, 1};
-//        int[] values = {10, 15, 16, 11, 12, 7};
-
-        short[] weights = {1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2};
-        int[] values = {10, 20, 10, 11, 12, 12, 8, 17, 11, 8, 7, 5, 5, 2, 17, 15, 14, 20, 22, 21};
+        short[] weights = { 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2 };
+        int[] values = { 10, 20, 10, 11, 12, 12, 8, 17, 11, 8, 7, 5, 5, 2, 17, 15, 14, 20, 22, 50 };
 
         for (int i = 0; i < weights.length; i++) {
             items.add(new Item(weights[i], values[i]));
         }
         return items;
     }
-
 
 }
 
@@ -119,6 +129,10 @@ class Item {
         this.value = value;
     }
 
+    public boolean isOfWeight(int weight) {
+        return this.weight == (short) weight;
+    }
+
     public short getWeight() {
         return weight;
     }
@@ -129,8 +143,10 @@ class Item {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
         Item item = (Item) o;
         return weight == item.weight && value == item.value;
     }
